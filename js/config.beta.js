ba323362999ed7c8f09c5a61e7378fdef1b443cb
HomareTheTwins <homare.s.t@gmail.com>
2026-06-08 18:19:14 +0900

    【変更】beta版のバージョンをv1.2に更新。 【変更】service-worker.beta.jsのキャッシュ名も更新。 【修正】public版のバージョン情報を設定画面に表示するように変更。

--- js/config.beta.js
+++ js/config.beta.js
@@ -1,6 +1,6 @@
 const APP_CONFIG = {
 	APP_NAME: "ST-Insight Beta",
-	APP_VERSION: "v0.35",
+	APP_VERSION: "v1.2",
 	ENV: "beta",
 	ENV_LABEL: "ST-Insight Beta"
 }
--- js/config.public.js
+++ js/config.public.js
@@ -2,5 +2,5 @@ const APP_CONFIG = {
 	APP_NAME: "ST-Insight",
 	APP_VERSION: "v0.35",
 	ENV: "public",
-	ENV_LABEL: ""	// 公開用は環境表示なし
+	ENV_LABEL: "public"	// 公開用は環境表示なし
 }
