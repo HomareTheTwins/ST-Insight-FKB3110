7df0301a089bcffd4cc9e257c776bdf6e24abc94
HomareTheTwins <homare.s.t@gmail.com>
2026-06-18 19:17:44 +0900

    【修正】ドロップ/ツイストボタンをドロップへ

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "ST-Insight-dev-v1.01";
-const BASE_PATH = "/ST-Insight-Dev/";
+const CACHE_NAME = "ST-Insight-beta-v0.9.0";
+const BASE_PATH = "/ST-Insight-Beta/";
 
 const urlsToCache = [
   BASE_PATH,
